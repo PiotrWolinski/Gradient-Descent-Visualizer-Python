662337cb678ca25b530d82e2f83d02cb5218a070
Piotr Woliński <piotr.wolinski.2017@outlook.com>
2021-12-31 18:00:07 +0100

    Last commit in this year. Polynomials are now shown correctly, but domain needs to be fixed

--- main.py
+++ main.py
@@ -1,40 +1,44 @@
 from matplotlib import pyplot as plt
+from matplotlib import cm
 import numpy as np
 import polynomial as poly 
 
-def plot_3D(f_x, f_y, f_z):
-
-    f = plt.figure()
+def plot_3D(f_x, f_y):
 
     ax = plt.axes(projection='3d')
-    x = f_x.tolist()
-    # print(x)
-    y = f_y.tolist()
-    # print(y)
-    z = f_z.tolist()
-    ax.scatter(x, y, z)
-    plt.xlabel('x')
-    plt.ylabel('y')
-    # plt.label('z')
-    # plt.title('Time comparison between Jacobi and Gauss-Seidl methods')
-    # plt.legend()
+
+    X = np.linspace(-100, 100, 1001)
+    Y = np.linspace(-100, 100, 1001)
+    X, Y = np.meshgrid(X, Y)
+
+    Z = np.zeros(shape=(1001, 1001))
+
+    for i in range(Z.shape[0]):
+        for j in range(Z.shape[1]):
+            Z[i, j] = f_x[i] + f_y[j]
+
+
+    ax.plot_surface(X, Y, Z, cmap=cm.coolwarm,
+                       linewidth=0, antialiased=False)
+
+    ax.set_xlabel('x')
+    ax.set_ylabel('y')
+    ax.set_zlabel('z')
+
     plt.show()
 
 def main():
-    f1 = poly.create(degree=4)
-    f2 = poly.create(degree=4)
-    f3 = poly.create(degree=4)
+    f1 = poly.create(degree=2)
+    f2 = poly.create(degree=2)
     print(f1)
     print(f2)
-    print(f3)
     f1_y, x = poly.get_values_in_domain(polynomial=f1)
     f2_y, y = poly.get_values_in_domain(polynomial=f2)
-    f3_y, z = poly.get_values_in_domain(polynomial=f3)
 
     print(f1_y)
     print(f2_y)
-    print(f3_y)
-    plot_3D(f1_y, f2_y, f3_y)
+    plot_3D(f1_y, f2_y)
 
 if __name__ == '__main__':
     main()
+    
--- polynomial.py
+++ polynomial.py
@@ -1,7 +1,7 @@
 import numpy as np
 
 
-def create(degree: int=2, low: int=-10, high: int=10) -> np.ndarray:
+def create(degree: int=2) -> np.ndarray:
     """Creates random polynomial of given degree
 
 
@@ -22,40 +22,32 @@ def create(degree: int=2, low: int=-10, high: int=10) -> np.ndarray:
 
     """
     size = degree + 1
-    coefs = np.random.randint(low=low, high=high, size=size, dtype=np.int32)
+    coefs = np.random.random_sample(size=size) * np.linspace(10, 1, num=size)
     return coefs
 
-def get_values_in_domain(polynomial: np.ndarray, low: int=-5, high: int=5, probes: int=1000) -> np.int32:
+def get_values_in_domain(polynomial: np.ndarray, low: int=-5, high: int=5, probes: int=1000):
     """Gets values of given polynomial in given (low, high) domain (low and high included)
     
     Keyword arguments:
     polynomial -- np.ndarray with coefficients describing this polynomial
-    Return: return_description
+    Return: Tuple containing (values, domain)
     """
-    domain_size = abs(low - high) 
-
     step = abs(low - high) / probes
 
     values = np.zeros(shape=probes+1, dtype=np.float32)
-    domain = np.arange(start=low, stop=high+step, step=step)
+    domain = np.linspace(start=low, stop=high, num=probes + 1)
 
     x = low
     id = 0
-
-    print(f'step = {step}')
-    print(f'size = {domain_size}')
 
     for x in domain:
         values[id] = get_value(polynomial, x)
         x += step
         id += 1
 
-    print(values.shape)
-    print(domain.shape)
-
     return values, domain
 
-def get_value(polynomial: np.ndarray, x: np.float32):
+def get_value(polynomial: np.ndarray, x: np.float32) -> np.float32:
     """Calculates value of given polynomial in point x
 
     
@@ -64,11 +56,9 @@ def get_value(polynomial: np.ndarray, x: np.float32):
 
     x -- np.float32 x value to calculate polynomial value
 
-
     Return: np.float32 value of given polynomial in point x 
     """
-    
-    value = 0.0
+    value = np.float32(0.0)
 
     degree = polynomial.shape[0] - 1
     current_id = 0
