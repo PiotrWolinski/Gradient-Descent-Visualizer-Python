be72c089d1d28e2763dce3898f07df84bd87be2b
Piotr Woliński <piotr.wolinski.2017@outlook.com>
2022-01-07 20:24:39 +0100

    Gradient descent is now visivle. Some tweaks needed.

--- main.py
+++ main.py
@@ -1,25 +1,31 @@
 from matplotlib import pyplot as plt
 from matplotlib import cm
 import numpy as np
-import polynomial as poly 
+import polynomial as poly
+from typing import Tuple
 
-def plot_3D(f_x, f_y):
+
+def plot_3D(polynomial_values, descent_path, domain) -> None:
 
     ax = plt.axes(projection='3d')
 
-    X = np.linspace(-100, 100, 1001)
-    Y = np.linspace(-100, 100, 1001)
+    low = domain[0]
+    high = domain[1]
+
+    X = np.linspace(low, high, 1001)
+    Y = np.linspace(low, high, 1001)
     X, Y = np.meshgrid(X, Y)
 
-    Z = np.zeros(shape=(1001, 1001))
+    descent_x = [x[0] for x in descent_path]
+    descent_y = [x[1] for x in descent_path]
+    descent_z = [x[2] for x in descent_path]
 
-    for i in range(Z.shape[0]):
-        for j in range(Z.shape[1]):
-            Z[i, j] = f_x[i] + f_y[j]
+    ax.scatter(descent_x, descent_y, descent_z, color=['red'], s=80)
+    ax.plot(descent_x, descent_y, descent_z, 'r')
 
+    ax.plot_surface(X, Y, polynomial_values, cmap=cm.coolwarm,
+                    linewidth=0, antialiased=False, alpha=0.4)
 
-    ax.plot_surface(X, Y, Z, cmap=cm.coolwarm,
-                       linewidth=0, antialiased=False)
 
     ax.set_xlabel('x')
     ax.set_ylabel('y')
@@ -27,18 +33,68 @@ def plot_3D(f_x, f_y):
 
     plt.show()
 
-def main():
-    f1 = poly.create(degree=2)
-    f2 = poly.create(degree=2)
-    print(f1)
-    print(f2)
-    f1_y, x = poly.get_values_in_domain(polynomial=f1)
-    f2_y, y = poly.get_values_in_domain(polynomial=f2)
 
-    print(f1_y)
-    print(f2_y)
-    plot_3D(f1_y, f2_y)
+def create_descent_path(
+        poly_x: np.ndarray, 
+        poly_y: np.ndarray, 
+        domain: np.ndarray, 
+        gradient_weight: float, 
+        iterations: int) -> np.ndarray:
+    derivative_x = poly.get_derivative(poly_x)
+    derivative_y = poly.get_derivative(poly_y)
+
+    start_x = domain[0] + np.random.rand() * (domain[-1] - domain[0])
+    start_y = domain[0] + np.random.rand() * (domain[-1] - domain[0])
+
+    polynomials_values = poly.get_multivariable_polynomial_values(poly_x, poly_y, domain)
+
+    start_value = poly.get_multivariable_polynomial_value(poly_x, poly_y, start_x, start_y)
+
+    descent_path = [(start_x, start_y, start_value)]
+
+    current_x = start_x
+    current_y = start_y
+
+    print(f'({current_x}, {current_y}')
+    for i in range(iterations):
+        # gradient = gradient_weight * poly.get_multivariable_polynomial_value(derivative_x, derivative_y, current_x, current_y)
+        gradient_x = poly.get_value(derivative_x, current_x)
+        gradient_y = poly.get_value(derivative_y, current_y)
+
+        current_x -= gradient_weight * gradient_x
+        current_y -= gradient_weight * gradient_y
+
+        descent_path.append((current_x, current_y, poly.get_multivariable_polynomial_value(poly_x, poly_y, current_x, current_y)))
+    
+        # print(gradient_x)
+        # print(gradient_y)
+
+        print(f'G=[{gradient_x}, {gradient_y}')
+
+        print(f'({current_x}, {current_y})')
+
+    print(descent_path)
+
+    return descent_path
+
+def main() -> None:
+    f_x = poly.create(degree=2)
+    f_y = poly.create(degree=2)
+
+    # print(f'f_x = {f_x}')
+    # print(f'f_y = {f_y}')
+
+    domain = (-5, 5)
+
+    polynomials_values = poly.get_multivariable_polynomial_values(f_x, f_y, domain)
+
+    gradient_weight = 0.01
+    iterations = 5
+
+    descent_path = create_descent_path(f_x, f_y, domain, gradient_weight, iterations)
+
+    plot_3D(polynomials_values, descent_path, domain)
+
 
 if __name__ == '__main__':
     main()
-    
--- polynomial.py
+++ polynomial.py
@@ -1,4 +1,5 @@
 import numpy as np
+from typing import Tuple
 
 
 def create(degree: int=2) -> np.ndarray:
@@ -22,10 +23,14 @@ def create(degree: int=2) -> np.ndarray:
 
     """
     size = degree + 1
-    coefs = np.random.random_sample(size=size) * np.linspace(10, 1, num=size)
+    coefs = np.random.random_sample(size=size) * np.linspace(10, 1, num=size) + np.linspace(1, 0, size)
     return coefs
 
-def get_values_in_domain(polynomial: np.ndarray, low: int=-5, high: int=5, probes: int=1000):
+def get_values_in_domain(
+        polynomial: np.ndarray, 
+        low: int=-5, 
+        high: int=5, 
+        probes: int=1000) -> Tuple[np.ndarray, np.ndarray]:
     """Gets values of given polynomial in given (low, high) domain (low and high included)
     
     Keyword arguments:
@@ -68,3 +73,53 @@ def get_value(polynomial: np.ndarray, x: np.float32) -> np.float32:
         current_id += 1
 
     return value
+
+def get_derivative(polynomial: np.ndarray) -> np.ndarray:
+    """Calculates derivative of given polynomial
+    
+    Keyword arguments:
+    polynomial -- polynomial in form of np.ndarray with coefficients in order from 
+    degree-1 to 0. Ie. [4, 5, 1] -> f(x) = 4 * x ^ 2 + 5 * x ^ 1 + 1 * x ^ 0
+
+    Then derivative of [4, 5, 1] will be [8, 5] -> f'(x) = 8 * x + 5
+
+    Return: derivative as np.ndarray of coefficients
+    """
+    
+    size = polynomial.shape[0]
+
+    if size < 2:
+        return np.zeros(shape=1)
+
+    derivative = polynomial[:-1:] * np.arange(start=size-1, stop=0, step=-1)
+
+    return derivative
+
+def get_multivariable_polynomial_values(poly_1: np.ndarray, poly_2: np.ndarray, domain: Tuple[int, int]) -> np.ndarray:
+    """Returns matrix representing values of sum of the two given polynomials
+    
+    Keyword arguments:
+    poly_1, poly_2  --  polynomials to get value from
+    domain  --  tuple of domain boundaries
+
+
+    Return: 2 dimensional np.ndarray with sum of these polynomials in given domain
+    """
+    domain_low, domain_high = domain
+    
+    values_1, _ = get_values_in_domain(
+        polynomial=poly_1, low=domain_low, high=domain_high)
+    values_2, _ = get_values_in_domain(
+        polynomial=poly_2, low=domain_low, high=domain_high)
+    
+    Z = np.zeros(shape=(1001, 1001))
+
+    for i in range(Z.shape[0]):
+        for j in range(Z.shape[1]):
+            Z[i, j] = values_1[i] + values_2[j]
+
+    return Z
+
+def get_multivariable_polynomial_value(poly_1, poly_2, x, y):
+
+    return get_value(poly_1, x) + get_value(poly_2, y)
