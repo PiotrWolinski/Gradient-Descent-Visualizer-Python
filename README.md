b8bfbef7fe9f496d603f2ad6d7d20686280f515c
Piotr Woliński <piotr.wolinski.2017@outlook.com>
2022-01-09 23:49:06 +0100

    Fixed descent path. Added readme, but refactor is needed.

---
+++ README.md
@@ -0,0 +1,9 @@
+# [Gradient Descent](https://en.wikipedia.org/wiki/Gradient_descent) Visualizer
+
+![Gradient descent path](graph.jpg "Gradient descent path")  
+
+Simple visualizer of what gradient descent actually means.  
+At this moment prepared for 3D graphs, but there will also be a 2D version.  
+
+Install requirements with:  
+`pip install -r requirements.txt`
--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import polynomial as poly
 from typing import Tuple
 
 
-def plot_3D(polynomial_values, descent_path, domain) -> None:
+def plot_3D_gradient_descent(f_x, f_y, descent_path, domain) -> None:
 
     ax = plt.axes(projection='3d')
 
@@ -16,6 +16,13 @@ def plot_3D(polynomial_values, descent_path, domain) -> None:
     Y = np.linspace(low, high, 1001)
     X, Y = np.meshgrid(X, Y)
 
+    Z = np.zeros(shape=(1001, 1001))
+
+    for i in range(Z.shape[0]):
+        for j in range(Z.shape[1]):
+            Z[i, j] = poly.get_multivariable_polynomial_value(
+                f_x, f_y, X[i, j], Y[i, j])
+
     descent_x = [x[0] for x in descent_path]
     descent_y = [x[1] for x in descent_path]
     descent_z = [x[2] for x in descent_path]
@@ -23,9 +30,8 @@ def plot_3D(polynomial_values, descent_path, domain) -> None:
     ax.scatter(descent_x, descent_y, descent_z, color=['red'], s=80)
     ax.plot(descent_x, descent_y, descent_z, 'r')
 
-    ax.plot_surface(X, Y, polynomial_values, cmap=cm.coolwarm,
+    ax.plot_surface(X, Y, Z, cmap=cm.coolwarm,
                     linewidth=0, antialiased=False, alpha=0.4)
-
 
     ax.set_xlabel('x')
     ax.set_ylabel('y')
@@ -35,65 +41,53 @@ def plot_3D(polynomial_values, descent_path, domain) -> None:
 
 
 def create_descent_path(
-        poly_x: np.ndarray, 
-        poly_y: np.ndarray, 
-        domain: np.ndarray, 
-        gradient_weight: float, 
+        poly_x: np.ndarray,
+        poly_y: np.ndarray,
+        domain: np.ndarray,
+        gradient_weight: float,
         iterations: int) -> np.ndarray:
-    derivative_x = poly.get_derivative(poly_x)
-    derivative_y = poly.get_derivative(poly_y)
 
     start_x = domain[0] + np.random.rand() * (domain[-1] - domain[0])
     start_y = domain[0] + np.random.rand() * (domain[-1] - domain[0])
 
-    polynomials_values = poly.get_multivariable_polynomial_values(poly_x, poly_y, domain)
+    start_value = poly.get_multivariable_polynomial_value(
+        poly_x, poly_y, start_x, start_y)
 
-    start_value = poly.get_multivariable_polynomial_value(poly_x, poly_y, start_x, start_y)
+    derivative_x = poly.get_derivative(poly_x)
+    derivative_y = poly.get_derivative(poly_y)
 
     descent_path = [(start_x, start_y, start_value)]
 
     current_x = start_x
     current_y = start_y
 
-    print(f'({current_x}, {current_y}')
+    print(f'({current_x}, {current_y})')
     for i in range(iterations):
-        # gradient = gradient_weight * poly.get_multivariable_polynomial_value(derivative_x, derivative_y, current_x, current_y)
         gradient_x = poly.get_value(derivative_x, current_x)
         gradient_y = poly.get_value(derivative_y, current_y)
 
         current_x -= gradient_weight * gradient_x
         current_y -= gradient_weight * gradient_y
 
-        descent_path.append((current_x, current_y, poly.get_multivariable_polynomial_value(poly_x, poly_y, current_x, current_y)))
-    
-        # print(gradient_x)
-        # print(gradient_y)
-
-        print(f'G=[{gradient_x}, {gradient_y}')
-
-        print(f'({current_x}, {current_y})')
-
-    print(descent_path)
+        descent_path.append((current_x, current_y, poly.get_multivariable_polynomial_value(
+            poly_x, poly_y, current_x, current_y)))
 
     return descent_path
+
 
 def main() -> None:
     f_x = poly.create(degree=2)
     f_y = poly.create(degree=2)
 
-    # print(f'f_x = {f_x}')
-    # print(f'f_y = {f_y}')
-
     domain = (-5, 5)
-
-    polynomials_values = poly.get_multivariable_polynomial_values(f_x, f_y, domain)
 
     gradient_weight = 0.01
     iterations = 5
 
-    descent_path = create_descent_path(f_x, f_y, domain, gradient_weight, iterations)
+    descent_path = create_descent_path(
+        f_x, f_y, domain, gradient_weight, iterations)
 
-    plot_3D(polynomials_values, descent_path, domain)
+    plot_3D_gradient_descent(f_x, f_y, descent_path, domain)
 
 
 if __name__ == '__main__':
